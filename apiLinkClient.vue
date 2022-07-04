d6b3cc3cf8580b607ca0922cb70cc0d8656073e3
tak-tezk <85788325+tak-tezk@users.noreply.github.com>
2022-07-04 16:23:12 +0900

    Create apiLinkClient.vue

---
+++ apiLinkClient.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <button @click="getData">ボタン</button>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+  data () {
+    return {
+        msg: "null"
+    };
+  },
+  methods: {
+    getData() {
+      this.$axios.get('http://localhost:8080/test').then(res => {
+        // リクエスト成功時
+        this.msg = res;
+      }).catch()
+    },
+  },
+})
+</script>
